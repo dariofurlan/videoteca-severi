0d2acd2889ffdc2603c4adfde19371b5b6fb2b74
MattiaZanellato <mattia.zanellato00@gmail.com>
2019-03-23 09:46:45 +0100

    Create admin_api.php

---
+++ backend/admin/admin_api.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if (!isset($_SESSION["email"]) || empty($_SESSION["email"])) {
+	header("Location: index.php");
+  exit();
+}
+require_once 'request.php';
+require_once 'response.php';
+
+$res = new Response();
+$req = new Request();
+if ($req->check_path()) {
+    $resource = $req->get_path();
+    if ($req->check_GET()) {
+      $cleaned_GET = $req->get_GET();
+      try {
+        require_once 'database.php';
+        $db = new Database();
+        $db->prepare_query($resource, $cleaned_GET);
+      } catch(Exception $e) {
+        $res->error(500);
+      }
+    } else {
+        $res->error(400);
+    }
+} else {
+    $res->error(404);
+}
